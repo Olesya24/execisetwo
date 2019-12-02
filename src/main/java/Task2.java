fcd0eaf823426a74634ce75a19e8b38a428b24c5
Олеся <olesyasashko24@gmail.com>
2019-12-02 16:13:30 +0300

    1

--- src/main/java/Task2.java
+++ src/main/java/Task2.java
@@ -44,9 +44,9 @@ public class Task2 {
         return -1;
     }
 
-    //We'll say that a String is xy-balanced if for all the 'x' chars in the string, there exists a 'y' char somewhere later in the string.
-    // So "xxy" is balanced, but "xyx" is not. One 'y' can balance multiple 'x's.
-    // Return true if the given string is xy-balanced.
+    /*We'll say that a String is xy-balanced if for all the 'x' chars in the string, there exists a 'y' char somewhere later in the string.
+    So "xxy" is balanced, but "xyx" is not. One 'y' can balance multiple 'x's.
+    Return true if the given string is xy-balanced.*/
 
     public boolean xyBalance(String str) {
         char ch;
@@ -57,6 +57,98 @@ public class Task2 {
                 return false;
             else if(ch == 'y')
                 return true;
+        }
+        return true;
+    }
+
+    /*Given a string, look for a mirror image (backwards) string at both the beginning and end of the given string.
+     In other words, zero or more characters at the very begining of the given string, and at the very end of the string
+     in reverse order (possibly overlapping). For example, the string "abXYZba" has the mirror end "ab".*/
+
+    public String mirrorEnds(String string) {
+        int p = string.length() - 1;
+        for(int i = 0; i < string.length(); i++, p--)
+        {
+            if(string.charAt(i) != string.charAt(p))
+                return string.substring(0, i);
+        }
+        return string;
+    }
+
+    /*Return an array that is "left shifted" by one -- so {6, 2, 5, 3} returns {2, 5, 3, 6}.
+    You may modify and return the given array, or return a new array.*/
+
+    public int[] shiftLeft(int[] nums) {
+        if(nums.length >= 2)
+        {
+            int temp = nums[0];
+            for(int i = 0; i < nums.length - 1; i++)
+                nums[i] = nums[i+1];
+            nums[nums.length-1] = temp;
+        }
+        return nums;
+    }
+
+    /*Given a non-empty array, return true if there is a place to split the array so that the sum of the numbers on one
+    side is equal to the sum of the numbers on the other side.
+     */
+    public boolean canBalance(int[] nums) {
+        int left = 0;
+        int right;
+        for(int i = 0; i < nums.length - 1; i++)
+            left += nums[i];
+        right = nums[nums.length-1];
+        for(int i = nums.length - 2; i > 0; i--)
+        {
+            if(left == right)
+                return true;
+            left -= nums[i];
+            right += nums[i];
+        }
+        return (left == right);
+    }
+
+    /*Given an int array length 3, if there is a 2 in the array immediately followed by a 3, set the 3 element to 0.
+    Return the changed array.
+     */
+
+    public int[] fix23(int[] nums) {
+        for (int i=0; i<nums.length-1;i++){
+            if (nums[i]==2 && nums[i+1]==3) {
+                nums[i+1]=0;
+            }
+        }
+        return nums;
+    }
+
+
+    /*We'll say that a lowercase 'g' in a string is "happy" if there is another 'g' immediately to its left or right.
+    Return true if all the g's in the given string are happy.
+     */
+    public boolean gHappy(String str) {
+        int check = 0;
+        for(int i = 0; i < str.length(); i++)
+        {
+            if(str.charAt(i) == 'g')
+            {
+                check = 0;
+                if(i > 0)
+                {
+                    if(str.charAt(i-1) != 'g')
+                        check++;
+                }
+                else
+                    check++;
+                if(i < str.length()-1)
+                {
+                    if(str.charAt(i+1) != 'g')
+                        check++;
+                }
+                else
+                    check++;
+                if(check == 2)
+                    return false;
+            }
         }
         return true;
     }
--- src/test/java/Task2Test.java
+++ src/test/java/Task2Test.java
@@ -9,13 +9,13 @@ public class Task2Test {
     public void nonStartTest() throws Exception {
         //GIVEN
         Task2 string= new Task2();
-        //WHEN
-        String actualString1 = string.nonStart("","");
-        String actualString2 = string.nonStart("a", "x");
-        String actualString3 = string.nonStart("ab", "xy");
-        String actualString4 = string.nonStart("ab", "x");
-        String actualString5 = string.nonStart("x", "ac");
-        String actualString6 = string.nonStart("java", "core");
+        //WHERE
+        String actual1 = string.nonStart("","");
+        String actual2 = string.nonStart("a", "x");
+        String actual3 = string.nonStart("ab", "xy");
+        String actual4 = string.nonStart("ab", "x");
+        String actual5 = string.nonStart("x", "ac");
+        String actual6 = string.nonStart("java", "core");
 
         String expected1 = "";
         String expected2 = "";
@@ -26,12 +26,12 @@ public class Task2Test {
 
 
         //THEN
-        assertEquals(expected1,actualString1);
-        assertEquals(expected2,actualString2);
-        assertEquals(expected3,actualString3);
-        assertEquals(expected4,actualString4);
-        assertEquals(expected5,actualString5);
-        assertEquals(expected6,actualString6);
+        assertEquals(expected1,actual1);
+        assertEquals(expected2,actual2);
+        assertEquals(expected3,actual3);
+        assertEquals(expected4,actual4);
+        assertEquals(expected5,actual5);
+        assertEquals(expected6,actual6);
     }
 
 
@@ -40,19 +40,19 @@ public class Task2Test {
         //GIVEN
         Task2 nums=new Task2();
 
-        //THEN
-        int[] actualnums1 = {};
-        int[] actualnums2 = {24};
-        int[] actualnums3 = {8, 8} ;
-        int[] actualnums4 = {1, 2, 3, 4, 5, 13, 1};
-        int[] actualnums5 = {1, 2, 3, 4, 5, 13};
+        //WHERE
+        int[] actual1 = {};
+        int[] actual2 = {24};
+        int[] actual3 = {8, 8} ;
+        int[] actual4 = {1, 2, 3, 4, 5, 13, 1};
+        int[] actual5 = {1, 2, 3, 4, 5, 13};
 
-        //WHEN
-        assertEquals(false, nums.sameFirstLast(actualnums1));
-        assertEquals(true, nums.sameFirstLast(actualnums2));
-        assertEquals(true,nums.sameFirstLast(actualnums3));
-        assertEquals(true,nums.sameFirstLast(actualnums4));
-        assertEquals(false, nums.sameFirstLast(actualnums5));
+        //THEN
+        assertEquals(false, nums.sameFirstLast(actual1));
+        assertEquals(true, nums.sameFirstLast(actual2));
+        assertEquals(true,nums.sameFirstLast(actual3));
+        assertEquals(true,nums.sameFirstLast(actual4));
+        assertEquals(false, nums.sameFirstLast(actual5));
 
     }
 
@@ -61,21 +61,21 @@ public class Task2Test {
         //GIVEN
         Task2 number = new Task2();
 
-        //THEN
-        boolean actualNumber1 = number.more20(0);
-        boolean actualNumber2 = number.more20(20);
-        boolean actualNumber3 = number.more20(61);
-        boolean actualNumber4 = number.more20(1021);
-        boolean actualNumber5 = number.more20(1000);
-        boolean actualNumber6 = number.more20(-1);
+        //WHERE
+        boolean actual1 = number.more20(0);
+        boolean actual2 = number.more20(20);
+        boolean actual3 = number.more20(61);
+        boolean actual4 = number.more20(1021);
+        boolean actual5 = number.more20(1000);
+        boolean actual6 = number.more20(-1);
 
-        //WHEN
-        assertFalse(actualNumber1);
-        assertFalse(actualNumber2);
-        assertTrue(actualNumber3);
-        assertTrue(actualNumber4);
-        assertFalse(actualNumber5);
-        assertFalse(actualNumber6);
+        //THEN
+        assertFalse(actual1);
+        assertFalse(actual2);
+        assertTrue(actual3);
+        assertTrue(actual4);
+        assertFalse(actual5);
+        assertFalse(actual6);
 
     }
 
@@ -84,11 +84,11 @@ public class Task2Test {
         //GIVEN
         Task2 bar = new Task2();
 
-        //THEN
-        int actualbar1 = bar.makeChocolate(4, 1, 9);
-        int actualbar2 = bar.makeChocolate(1, 2, 5);
-        int actualbar3 = bar.makeChocolate(4, 1, 10);
-        int actualbar4 = bar.makeChocolate(60, 100, 550);
+        //WHERE
+        int actual1 = bar.makeChocolate(4, 1, 9);
+        int actual2 = bar.makeChocolate(1, 2, 5);
+        int actual3 = bar.makeChocolate(4, 1, 10);
+        int actual4 = bar.makeChocolate(60, 100, 550);
 
 
         int expected1 = 4;
@@ -96,11 +96,11 @@ public class Task2Test {
         int expected3 = -1;
         int expected4 = 50;
 
-        //WHEN
-        assertEquals(expected1, actualbar1);
-        assertEquals(expected2, actualbar2);
-        assertEquals(expected3, actualbar3);
-        assertEquals(expected4, actualbar4);
+        //THEN
+        assertEquals(expected1, actual1);
+        assertEquals(expected2, actual2);
+        assertEquals(expected3, actual3);
+        assertEquals(expected4, actual4);
     }
 
 
@@ -109,22 +109,152 @@ public class Task2Test {
         //GIVEN
         Task2 balance = new Task2();
 
-        //THEN
-        boolean actualBalance1 = balance.xyBalance("");
-        boolean actualBalance2 = balance.xyBalance("y");
-        boolean actualBalance3 = balance.xyBalance("x");
-        boolean actualBalance4 = balance.xyBalance("xy");
-        boolean actualBalance5 = balance.xyBalance("xaxxbby");
-        boolean actualBalance6 = balance.xyBalance("xaxxbbyx");
-        boolean actualBalance7 = balance.xyBalance("12xabxxydxyxyzz");
+        //WHERE
+        boolean actual1 = balance.xyBalance("");
+        boolean actual2 = balance.xyBalance("y");
+        boolean actual3 = balance.xyBalance("x");
+        boolean actual4 = balance.xyBalance("xy");
+        boolean actual5 = balance.xyBalance("xaxxbby");
+        boolean actual6 = balance.xyBalance("xaxxbbyx");
+        boolean actual7 = balance.xyBalance("12xabxxydxyxyzz");
 
-        //WHEN
-        assertTrue(actualBalance1);
-        assertTrue(actualBalance2);
-        assertFalse(actualBalance3);
-        assertTrue(actualBalance4);
-        assertTrue(actualBalance5);
-        assertFalse(actualBalance6);
-        assertTrue(actualBalance7);
+        //THEN
+        assertTrue(actual1);
+        assertTrue(actual2);
+        assertFalse(actual3);
+        assertTrue(actual4);
+        assertTrue(actual5);
+        assertFalse(actual6);
+        assertTrue(actual7);
+    }
+
+    @Test
+    public void mirrorEndsTest() {
+        //GIVEN
+        Task2 mirror = new Task2();
+
+        //WHERE
+        String actual1 = mirror.mirrorEnds("");
+        String actual2 = mirror.mirrorEnds("k");
+        String actual3 = mirror.mirrorEnds("aba");
+        String actual4 = mirror.mirrorEnds("band andab");
+        String actual5 = mirror.mirrorEnds("abab");
+
+        String expected1 = "";
+        String expected2 = "k";
+        String expected3 = "aba";
+        String expected4 = "ba";
+        String expected5 = "";
+
+        //THEN
+        assertEquals(expected1, actual1);
+        assertEquals(expected2, actual2);
+        assertEquals(expected3, actual3);
+        assertEquals(expected4, actual4);
+        assertEquals(expected5, actual5);
+
+    }
+
+    @Test
+    public void shiftLeftTest() {
+        //GIVEN
+        Task2 nums=new Task2();
+
+        //WHERE
+        int[] a1 = {};
+        int[] a2 = {1};
+        int[] a3 = {1, 2};
+        int[] a4 = {1, 1, 2, 2, 4};
+
+        int[] actual1 = nums.shiftLeft(a1);
+        int[] actual2 = nums.shiftLeft(a2);
+        int[] actual3 = nums.shiftLeft(a3);
+        int[] actual4 = nums.shiftLeft(a4);
+
+        int[] expected1 = {};
+        int[] expected2 = {1};
+        int[] expected3 = {2, 1};
+        int[] expected4 = {1, 2, 2, 4, 1};
+
+        //THEN
+        assertEquals(expected1, actual1);
+        assertEquals(expected2, actual2);
+        assertEquals(expected3, actual3);
+        assertEquals(expected4, actual4);
+    }
+
+    @Test
+    public void canBalanceTest() {
+        //GIVEN
+        Task2 nums=new Task2();
+
+        //WHERE
+        int[] actual1 = {24};
+        int[] actual3 = {8, 8};
+        int[] actual4 = {1, 2, 3, 1, 0, 1, 3};
+        int[] actual5 = {1, 2, 3, 1, 0, 2, 3};
+        int[] actual6 = {10, 0, 1, -1, 10};
+
+        //THEN
+        assertEquals(false, nums.canBalance(actual1));
+        assertEquals(true, nums.canBalance(actual3));
+        assertEquals(false, nums.canBalance(actual4));
+        assertEquals(true, nums.canBalance(actual5));
+        assertEquals(true, nums.canBalance(actual6));
+    }
+
+    @Test
+    public void fix23Test() {
+        //GIVEN
+        Task2 nums=new Task2();
+
+        //WHERE
+        int[] a1 = {1, 2, 3};
+        int[] a2 = {2, 3, 5};
+        int[] a3 = {3, 2, 1};
+        int[] a4 = {1, 2, 1};
+        int[] a5 = {2, 3, 3};
+
+        int[] actual1 = nums.fix23(a1);
+        int[] actual2 = nums.fix23(a2);
+        int[] actual3 = nums.fix23(a3);
+        int[] actual4 = nums.fix23(a4);
+        int[] actual5 = nums.fix23(a5);
+
+        int[] expected1 = {1, 2, 0};
+        int[] expected2 = {2, 0, 5};
+        int[] expected3 = {3, 2, 1};
+        int[] expected4 = {1, 2, 1};
+        int[] expected5 = {2, 0, 3};
+
+        //THEN
+        assertEquals(expected1, actual1);
+        assertEquals(expected2, actual2);
+        assertEquals(expected3, actual3);
+        assertEquals(expected4, actual4);
+        assertEquals(expected5, actual5);
+    }
+
+
+    @Test
+    public void gHappyTest() {
+        //GIVEN
+        Task2 happy = new Task2();
+
+        //WHERE
+        boolean actual1 = happy.gHappy("");
+        boolean actual2 = happy.gHappy("g");
+        boolean actual3 = happy.gHappy("gg");
+        boolean actual4 = happy.gHappy("oooggooo");
+        boolean actual5 = happy.gHappy("ooogooo");
+        boolean actual6 = happy.gHappy("oooggoogo");
+
+        //THEN
+        assertTrue(actual1);
+        assertFalse(actual2);
+        assertTrue(actual3);
+        assertTrue(actual4);
+        assertFalse(actual5);
+        assertFalse(actual6);
     }
 }
