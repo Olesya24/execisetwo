5464aaf1f288ed793e8585eb27f053805c7e0865
Олеся <olesyasashko24@gmail.com>
2019-12-02 05:54:54 +0300

    1

---
+++ src/main/java/Task2.java
@@ -0,0 +1,63 @@
+public class Task2 {
+    //Given 2 strings, return their concatenation, except omit the first char of each. The strings will be at least length 1.
+
+    public String nonStart(String a, String b) {
+        if (a.length()>=1 && b.length()>=1) {
+        return (a.substring(1) + b.substring(1));
+        }
+        return "";
+    }
+
+    //Given an array of ints, return true if the array is length 1 or more, and the first element and the last element are equal.
+
+    public boolean sameFirstLast(int[] nums) {
+        if (nums.length>=1){
+            if (nums[0]==nums[nums.length-1]) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // Return true if the given non-negative number is 1 or 2 more than a multiple of 20.
+
+    public boolean more20(int n) {
+        if (n >= 0) {
+        if (n%20==1 || n%20==2) {
+            return true;
+        }
+        }
+        return false;
+    }
+
+    //We want make a package of goal kilos of chocolate. We have small bars (1 kilo each) and big bars (5 kilos each).
+    // Return the number of small bars to use, assuming we always use big bars before small bars. Return -1 if it can't be done.
+
+    public int makeChocolate(int small, int big, int goal) {
+        int maxBig = goal/5;
+        if(maxBig <= big)
+            goal -= maxBig*5;
+        else
+            goal -= big*5;
+        if(goal <= small)
+            return goal;
+        return -1;
+    }
+
+    //We'll say that a String is xy-balanced if for all the 'x' chars in the string, there exists a 'y' char somewhere later in the string.
+    // So "xxy" is balanced, but "xyx" is not. One 'y' can balance multiple 'x's.
+    // Return true if the given string is xy-balanced.
+
+    public boolean xyBalance(String str) {
+        char ch;
+        for(int i = str.length() - 1; i >= 0; i--)
+        {
+            ch = str.charAt(i);
+            if(ch == 'x')
+                return false;
+            else if(ch == 'y')
+                return true;
+        }
+        return true;
+    }
+}
---
+++ src/test/java/Task2Test.java
@@ -0,0 +1,130 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class Task2Test {
+
+    @Test
+    public void nonStartTest() throws Exception {
+        //GIVEN
+        Task2 string= new Task2();
+        //WHEN
+        String actualString1 = string.nonStart("","");
+        String actualString2 = string.nonStart("a", "x");
+        String actualString3 = string.nonStart("ab", "xy");
+        String actualString4 = string.nonStart("ab", "x");
+        String actualString5 = string.nonStart("x", "ac");
+        String actualString6 = string.nonStart("java", "core");
+
+        String expected1 = "";
+        String expected2 = "";
+        String expected3 = "by";
+        String expected4 = "b";
+        String expected5 = "c";
+        String expected6= "avaore";
+
+
+        //THEN
+        assertEquals(expected1,actualString1);
+        assertEquals(expected2,actualString2);
+        assertEquals(expected3,actualString3);
+        assertEquals(expected4,actualString4);
+        assertEquals(expected5,actualString5);
+        assertEquals(expected6,actualString6);
+    }
+
+
+    @Test
+    public void sameFirstLastTest() {
+        //GIVEN
+        Task2 nums=new Task2();
+
+        //THEN
+        int[] actualnums1 = {};
+        int[] actualnums2 = {24};
+        int[] actualnums3 = {8, 8} ;
+        int[] actualnums4 = {1, 2, 3, 4, 5, 13, 1};
+        int[] actualnums5 = {1, 2, 3, 4, 5, 13};
+
+        //WHEN
+        assertEquals(false, nums.sameFirstLast(actualnums1));
+        assertEquals(true, nums.sameFirstLast(actualnums2));
+        assertEquals(true,nums.sameFirstLast(actualnums3));
+        assertEquals(true,nums.sameFirstLast(actualnums4));
+        assertEquals(false, nums.sameFirstLast(actualnums5));
+
+    }
+
+    @Test
+    public void more20Test() {
+        //GIVEN
+        Task2 number = new Task2();
+
+        //THEN
+        boolean actualNumber1 = number.more20(0);
+        boolean actualNumber2 = number.more20(20);
+        boolean actualNumber3 = number.more20(61);
+        boolean actualNumber4 = number.more20(1021);
+        boolean actualNumber5 = number.more20(1000);
+        boolean actualNumber6 = number.more20(-1);
+
+        //WHEN
+        assertFalse(actualNumber1);
+        assertFalse(actualNumber2);
+        assertTrue(actualNumber3);
+        assertTrue(actualNumber4);
+        assertFalse(actualNumber5);
+        assertFalse(actualNumber6);
+
+    }
+
+    @Test
+    public void makeChocolateTest() {
+        //GIVEN
+        Task2 bar= new Task2();
+
+        //THEN
+        int actualbar1 = bar.makeChocolate(4, 1, 9);
+        int actualbar2 = bar.makeChocolate(1, 2, 5);
+        int actualbar3 = bar.makeChocolate(4, 1, 10);
+        int actualbar4 = bar.makeChocolate(60, 100, 550);
+
+
+        int expected1 = 4;
+        int expected2 = 0;
+        int expected3 = -1;
+        int expected4 = 50;
+
+        //WHEN
+        assertEquals(expected1, actualbar1);
+        assertEquals(expected2, actualbar2);
+        assertEquals(expected3, actualbar3);
+        assertEquals(expected4, actualbar4);
+    }
+
+
+    @Test
+    public void xyBalanceTest() {
+        //GIVEN
+        Task2 balance= new Task2();
+
+        //THEN
+        boolean actualBalance1 = balance.xyBalance("");
+        boolean actualBalance2 = balance.xyBalance("y");
+        boolean actualBalance3 = balance.xyBalance("x");
+        boolean actualBalance4 = balance.xyBalance("xy");
+        boolean actualBalance5 = balance.xyBalance("xaxxbby");
+        boolean actualBalance6 = balance.xyBalance("xaxxbbyx");
+        boolean actualBalance7 = balance.xyBalance("12xabxxydxyxyzz");
+
+        //WHEN
+        assertTrue(actualBalance1);
+        assertTrue(actualBalance2);
+        assertFalse(actualBalance3);
+        assertTrue(actualBalance4);
+        assertTrue(actualBalance5);
+        assertFalse(actualBalance6);
+        assertTrue(actualBalance7);
+    }
+}
